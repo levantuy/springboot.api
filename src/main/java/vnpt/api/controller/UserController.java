00bf6ff8343183365218ded75bfa38087766e87a
levantuy <levantuy.it@gmail.com>
2018-09-28 11:12:37 +0700

    get me

--- src/main/java/vnpt/api/controller/UserController.java
+++ src/main/java/vnpt/api/controller/UserController.java
@@ -3,11 +3,17 @@ package vnpt.api.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import vnpt.api.payload.UserSummary;
+import vnpt.api.security.CurrentUser;
+import vnpt.api.security.UserPrincipal;
+
 import vnpt.api.exception.ResourceNotFoundException;
 import vnpt.api.model.User;
 import vnpt.api.payload.UserProfile;
@@ -21,6 +27,14 @@ public class UserController {
 	private UserRepository userRepository;
 
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+	@GetMapping("/user/me")
+	@PreAuthorize("hasRole('TEST')")
+	public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+		UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(),
+				currentUser.getName());
+		return userSummary;
+	}
 
 	@GetMapping("/user/checkUsernameAvailability")
 	public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
